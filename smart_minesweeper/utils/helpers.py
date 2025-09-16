2766f629202d4a96d9cb995c86e540208d304e4f
Gaganharikiranmarella <ghk7125@gmail.com>
2025-09-16 11:08:10 +0530

    made changes

--- smart_minesweeper/utils/helpers.py
+++
@@ -1,7 +0,0 @@
-def clamp(value, min_value, max_value):
-    """Clamp value between min_value and max_value."""
-    return max(min_value, min(value, max_value))
-
-def flatten_2d_list(lst):
-    """Flatten a 2D list into a 1D list."""
-    return [item for sublist in lst for item in sublist]
